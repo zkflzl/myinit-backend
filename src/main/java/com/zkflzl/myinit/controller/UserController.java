a656b76d46fa1c87f337dac16e525696888529f9
zkflzl <2158241973@11.com>
2023-05-31 16:48:53 +0800

    通用返回功能

--- src/main/java/com/zkflzl/myinit/controller/UserController.java
+++ src/main/java/com/zkflzl/myinit/controller/UserController.java
@@ -3,6 +3,7 @@ package com.zkflzl.myinit.controller;
 import com.zkflzl.myinit.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,6 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @GetMapping
-    public String testGetMsg() {
-        return "aaaaaaaaa";
-    }
+
 
 }

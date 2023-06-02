a5eb20afcf99e4e6bb0a1bcd5387c1c7daf94f08
zkflzl <2158241973@qq.com>
2023-06-02 19:22:26 +0800

    异常工具类

---
+++ src/main/java/com/zkflzl/myinit/exception/ThrowUtils.java
@@ -0,0 +1,44 @@
+package com.zkflzl.myinit.exception;
+
+import com.zkflzl.myinit.common.ErrorCode;
+
+/**
+ * 抛异常工具类
+ *
+ * @author <a href="https://github.com/zkflzl">程序员zk</a>
+ */
+public class ThrowUtils {
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param runtimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException) {
+        if (condition) {
+            throw runtimeException;
+        }
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     * @param message
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+}
